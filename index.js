a4cb86caa5d38f7eecbbecd86c7e21741b7bf7eb
unknown <abdullahs68400@gmail.com>
2024-05-02 20:24:00 +0300

    [PR-1] - java script button add

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
 const navbarMenu = document.querySelector(".hero-section-nav");
+const mainContent = document.querySelector(".hero-section-contents");
+const navbarButton = document.querySelector(".hs-getintouch-button");
+
+
     let isOpen = false;
     function toggleBtn() {
         if (isOpen) {
             navbarMenu.style.display = "flex";
+            mainContent.style.display = "none";
+            navbarButton.style.display = "inline-block";
+            document.body.style.overflow = "hidden";
             isOpen = !isOpen;
         } else {
             navbarMenu.style.display = "none";
+            mainContent.style.display = "inline-block";
+            navbarButton.style.display = "none";
+            document.body.style.overflow = "auto";
             isOpen = !isOpen;
         }
     
     }
  
 
-
-
-
-
-
-
-
-
-// const toggleMenu = document.querySelector(".menu-toggle");
-//  const navbarMenu = document.querySelector(".hero-section-nav");
-//  const navbarButton = document.querySelector(".hs-getintouch-button");
-
-//  let isOpen = false;
-
-//  toggleMenu.addEventListener("click", () => {    
-//      if (isOpen) {
-//          navbarMenu.style.display = "flex";
-//          navbarButton.style.display = "inline-block";
-//         isOpen = !isOpen;
-//      } else {
-//          navbarMenu.style.display = "none";
-//         navbarButton.style.display = "none";
-//         isOpen = !isOpen;
-//      }
-//  });
