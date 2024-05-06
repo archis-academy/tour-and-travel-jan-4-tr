f05b839b9283d2fb84ca05a6045b87403c3199df
FurkanOzpinar <frknzpnr@gmail.com>
2024-05-07 01:44:18 +0300

    PR-17 finished js, organized html and css files accordingly

--- packages.js
+++ packages.js
@@ -1,8 +1,17 @@
-const.infonav = document.querySelectorAll(".info-nav-btn");
+function openPage(elmnt, color) {
+    var infoNavBtn, i;
 
-for (let i = 0; i < infonav.length; i++) {
-    infonav[i].addEventListener("click", function chBackcolor(color) {
-        infonav[i].classList.toggle("info-nav-btn-active");
-        infonav[i].nextElementSibling.classList.toggle("info-nav-btn-active");
-    });
+    // Remove active class from all buttons
+    infoNavBtn = document.getElementsByClassName("info-nav-btn");
+    for (i = 0; i < infoNavBtn.length; i++) {
+        infoNavBtn[i].style.backgroundColor = "";
+    }
+
+    // Set background color of the clicked button
+    elmnt.style.backgroundColor = color;
 }
+
+// Code to click the default button when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("default-open").click();
+});
