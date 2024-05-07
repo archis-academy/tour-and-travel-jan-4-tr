683e87c1409b73ef7d6ad019e8e3de3b1abb38a6
unknown <abdullahs68400@gmail.com>
2024-05-07 18:05:05 +0300

    [PR-1] - hamburgermenu edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const navbarMenu = document.querySelector(".hero-section-nav");
 const mainContent = document.querySelector(".hero-section-contents");
-// const navbarButton = document.querySelector(".hs-getintouch-button");
+ const navbarButton = document.querySelector(".hs-getintouch-button");
 
 
     let isOpen = false;
@@ -8,13 +8,13 @@ const mainContent = document.querySelector(".hero-section-contents");
         if (isOpen) {
             navbarMenu.style.display = "flex";
             mainContent.style.display = "none";
-            // navbarButton.style.display = "inline-block";
+            navbarButton.style.display = "inline-block";
             document.body.style.overflow = "hidden";
             isOpen = !isOpen;
         } else {
             navbarMenu.style.display = "none";
             mainContent.style.display = "inline-block";
-            // navbarButton.style.display = "none";
+            navbarButton.style.display = "none";
             document.body.style.overflow = "auto";
             isOpen = !isOpen;
         }
