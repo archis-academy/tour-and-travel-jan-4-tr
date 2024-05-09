d520b67a35a06106672d34d00a0d034c169a53c8
unknown <abdullahs68400@gmail.com>
2024-05-09 23:52:08 +0300

    [PR-1] - navbarmenu and js edit

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const navbarMenu = document.querySelector(".hero-section-nav");
+const navbarMenu = document.querySelector("#section-nav");
 const mainContent = document.querySelector(".hero-section-contents");
- const navbarButton = document.querySelector(".hs-getintouch-button");
+const navbarButton = document.querySelector(".hs-getintouch-button");
 
+let isOpen = open;
+function toggleBtn() {
+  if (isOpen) {
+    navbarMenu.style.display = "flex";
+    mainContent.style.display = "none";
+    navbarButton.style.display = "inline-block";
+    document.body.style.overflow = "hidden";
+    isOpen = !isOpen;
+  } else {
+    navbarMenu.style.display = "none";
+    mainContent.style.display = "inline-block";
+    navbarButton.style.display = "none";
+    document.body.style.overflow = "auto";
+    isOpen = !isOpen;
+  }
+}
 
-    let isOpen = false;
-    function toggleBtn() {
-        if (isOpen) {
-            navbarMenu.style.display = "flex";
-            mainContent.style.display = "none";
-            navbarButton.style.display = "inline-block";
-            document.body.style.overflow = "hidden";
-            isOpen = !isOpen;
-        } else {
-            navbarMenu.style.display = "none";
-            mainContent.style.display = "inline-block";
-            navbarButton.style.display = "none";
-            document.body.style.overflow = "auto";
-            isOpen = !isOpen;
-        }
-    
-    }
- 
+function handleResize() {
+  const width = window.innerWidth;
 
+  console.log("Window width:", width);
+
+  if (width > 1000) {
+    navbarMenu.style.display = "flex";
+    navbarButton.style.display = "inline-block";
+    document.body.style.overflow = "auto";
+  } else {
+    navbarMenu.style.display = "none";
+    navbarButton.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
+window.addEventListener("resize", handleResize);
+handleResize();
